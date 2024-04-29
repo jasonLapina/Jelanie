9d01faa8f2fa321c030b09e62263799d69764802
jasonLapina <lapina.jason@gmail.com>
2024-04-30 01:03:37 +0800

    works masonry modal

---
+++ components/Works/MasonryModal.js
@@ -0,0 +1,45 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  useDisclosure,
+  Box,
+  Image,
+} from "@chakra-ui/react";
+
+function MasonryModal({ src, height }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box
+        mb='8px'
+        key={height}
+        bgColor='red'
+        h={height}
+        bgImage={`url(/assets/work_samples/${src})`}
+        bgSize='cover'
+        bgPos='top'
+        bgRepeat='no-repeat'
+        borderRadius='10px'
+        overflow='hidden'
+        cursor='pointer'
+        onClick={onOpen}
+      />
+      <Modal isOpen={isOpen} onClose={onClose} blockScrollOnMount={false}>
+        <ModalOverlay />
+        <ModalContent>
+          {/* <ModalHeader>Modal Title</ModalHeader> */}
+          <Image alt='work sample' src={`/assets/work_samples/${src}`} />
+          {/* <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter> */}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default MasonryModal;
--- components/Works/WorksMasonry.js
+++ components/Works/WorksMasonry.js
@@ -1,67 +1,72 @@
-import { AlertTitle, Box, Grid, Image, SimpleGrid } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import MasonryModal from "./MasonryModal";
 
 export default function WorksMasonry() {
   const masonItems = [
     {
       src: "5minSkin.png",
-      height: "200",
+      height: "300",
     },
     {
       src: "airstyle.jpg",
-      height: "230",
+      height: "250",
     },
     {
       src: "bezeli_march.jpg",
       height: "190",
     },
     {
-      src: "bezeli.jpg",
-      height: "170",
+      src: "daisy2.png",
+      height: "320",
     },
+
     {
       src: "bonsai.jpg",
       height: "210",
     },
     {
       src: "brute_force.png",
-      height: "250",
-    },
-    {
-      src: "buzzbud.jpg",
-      height: "180",
-    },
-    {
-      src: "cru.png",
-      height: "250",
-    },
-    {
-      src: "daisy.png",
-      height: "220",
-    },
-    {
-      src: "daisy2.png",
       height: "200",
     },
     {
+      src: "buzzbud.jpg",
+      height: "240",
+    },
+    {
+      src: "cru.png",
+      height: "210",
+    },
+    {
+      src: "bezeli.jpg",
+      height: "280",
+    },
+    {
+      src: "daisy.png",
+      height: "350",
+    },
+
+    {
       src: "gassy.jpg",
-      height: "180",
+      height: "220",
     },
   ];
   return (
     <Box my='120px' px='40px' style={{ columns: "400px auto" }}>
       {masonItems.map((m) => (
-        <Box
-          mb='8px'
-          key={m.src}
-          bgColor='red'
-          h={`${m.height}`}
-          bgImage={`url(/assets/work_samples/${m.src})`}
-          bgSize='cover'
-          bgPos='top'
-          bgRepeat='no-repeat'
-          borderRadius='10px'
-          overflow='hidden'
-        />
+        // <Box
+        //   mb='8px'
+        //   key={m.src}
+        //   bgColor='red'
+        //   h={`${m.height}`}
+        //   bgImage={`url(/assets/work_samples/${m.src})`}
+        //   bgSize='cover'
+        //   bgPos='top'
+        //   bgRepeat='no-repeat'
+        //   borderRadius='10px'
+        //   overflow='hidden'
+        //   cursor='pointer'
+        // />
+        <MasonryModal height={m.height} src={m.src} key={m.src} />
       ))}
     </Box>
   );
